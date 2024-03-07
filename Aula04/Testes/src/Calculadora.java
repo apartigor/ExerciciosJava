cb7259b2884502c3c152605b675e3ec22b40481f
Igor <141429429+apartigor@users.noreply.github.com>
2024-03-07 10:47:59 -0300

    Update Aula04

---
+++ Aula04/Testes/src/Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
--- Aula04/Testes/src/Programa01.java
+++ Aula04/Testes/src/Programa01.java
@@ -1,5 +1,20 @@
-public class Programa01 {
-    public static void main(String[] args) {
 
+public class Programa01 {
+
+    public static void imprimir(String str) {
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 2;
+
+        imprimir("A + B = " + Calculadora.somar(a, b));
+        imprimir("A - B = " + Calculadora.sub(a, b));
+        imprimir("A x B = " + Calculadora.mult(a, b));
+        imprimir("A / B = " + Calculadora.div(a, b));
+        imprimir("(A + B) x A = " + (Calculadora.somar(a, b) * a));
+        imprimir("A + B x A - (A + B) = " + ((Calculadora.mult(a, b) + a) - (Calculadora.somar(a, b))));
     }
 }
